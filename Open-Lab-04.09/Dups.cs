97ea5b093e6622913e813b781ab4c9913dbe33c5
romankalab <roan.kalab@gmail.com>
2019-11-07 09:18:48 +0000

    Initial

---
+++ Open-Lab-04.09/Dups.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace Open_Lab_04._09
+{
+    class Dups
+    {
+        public string[] RemoveDups(string[] arr)
+        {
+            return arr.Distinct().ToArray();
+        }
+    }
+}
---
+++ Open-Lab-04.09/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Open_Lab_04._09
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] arr = new string[4];
+            Console.WriteLine("Enter array elements:");
+            for (var x = 0; x < arr.Length; x++)
+            {
+                Console.WriteLine($"Enter {x + 1}. element:");
+                arr[x] = Console.ReadLine();
+            }
+
+            Dups dups = new Dups();
+            string[] result = dups.RemoveDups(arr);
+            foreach (var item in result)
+                Console.Write(item + ", ");
+        }
+    }
+}
